86b19d73da32f25a9b9b817dfb9a38b60eb5105f
Vladislav Valek <vladislav.valek@email.cz>
2023-03-28 18:10:46 +0200

    [FEATURE]: Initial commit
    Only copied source files from FB4CGG3 card.

---
+++ README.md
@@ -0,0 +1,16 @@
+# fb4CGg3@VU9P or fb2CGg3@VU9P for the NDK
+
+This repository contains an extension of the Network Development Kit (NDK) to support the **Silicom fb4CGg3@VU9P** or **Silicom fb2CGg3@VU9P** FPGA card.
+- The NDK allows users to quickly and easily develop new network applications based on FPGA acceleration cards.
+- You can build the FPGA firmware for this card using the [NDK-APP-Minimal application](https://github.com/CESNET/ndk-app-minimal/). The NDK-APP-Minimal is a reference application based on the NDK.
+- The [NDK-APP-Minimal documentation](https://cesnet.github.io/ndk-app-minimal/) lists steps for building the FPGA firmware, loading it into the FPGA card, and also using it in the chapter "How to start".
+
+## License
+
+Unless otherwise noted, the content of this repository is available under the BSD 3-Clause License. Please read [LICENSE file](LICENSE).
+
+- The .xci files located in the `src/ip/` folder were generated in the Xilinx Vivado, and their use may be subject to additional license agreements.
+
+## Repository Maintainer
+
+- Jakub Cabal, cabal@cesnet.cz
---
+++ src/Modules.tcl
@@ -0,0 +1,36 @@
+# Modules.tcl: script to compile card
+# Copyright (C) 2022 CESNET z. s. p. o.
+# Author(s): Jakub Cabal <cabal@cesnet.cz>
+#            Vladislav Valek <xvalek14@vutbr.cz>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# converting input list to associative array
+array set ARCHGRP_ARR $ARCHGRP
+
+# Paths
+set SPI_FLASH_DRIVER_BASE "$ENTITY_BASE/comp/spi_flash_driver/"
+set BOOT_CTRL_BASE        "$OFM_PATH/../core/intel/src/comp/boot_ctrl"
+set AXI2AVMM_BRIDGE_BASE  "$OFM_PATH/comp/mem_tools/convertors/axi2avmm_ddr_bridge"  
+
+# Components
+lappend COMPONENTS [list "FPGA_COMMON"      $ARCHGRP_ARR(CORE_BASE)  $ARCHGRP]
+lappend COMPONENTS [list "BOOT_CTRL"        $BOOT_CTRL_BASE          "FULL"  ]
+lappend COMPONENTS [list "SPI_FLASH_DRIVER" $SPI_FLASH_DRIVER_BASE   "FULL"  ]
+lappend COMPONENTS [list "AXI2AVMM_BRIDGE"  $AXI2AVMM_BRIDGE_BASE    "FULL"  ]
+
+# IP sources
+lappend MOD "$ENTITY_BASE/ip/cmac_eth_1x100g/cmac_eth_1x100g.xci"
+if {$ARCHGRP_ARR(PCIE_ENDPOINT_MODE) == 0} {
+    lappend MOD "$ENTITY_BASE/ip/pcie4_uscale_plus/x16/pcie4_uscale_plus.xci"
+} else {
+    lappend MOD "$ENTITY_BASE/ip/pcie4_uscale_plus/x8_low_latency/pcie4_uscale_plus.xci"
+}
+
+if {$ARCHGRP_ARR(XVC_ENABLE)} {
+    lappend MOD "$ENTITY_BASE/ip/xvc_vsec/xvc_vsec.xci"
+}
+
+# lappend MOD "$ENTITY_BASE/ip/ddr4_axi/ddr4_axi.xci"
+# Top-level
+lappend MOD "$ENTITY_BASE/fpga.vhd"
